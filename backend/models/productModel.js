5c8ae066581085b563217b1238972793139ace59
thiennhanng191 <ntnguyen@vassar.edu>
2021-02-11 11:59:12 -0500

    modelling data

---
+++ backend/models/productModel.js
@@ -0,0 +1,44 @@
+import mongoose from 'mongoose';
+
+const reviewSchema = mongoose.Schema({
+    name: { type: String, required: true },
+    rating: { type: Number },
+    comment: { type: String, required: true }
+}, {
+    timestamps: true
+});
+
+const productSchema = mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'User' // add relationship between Product and User
+    },
+    name: { type: String, required: true },
+    image: { type: String, required: true },
+    brand: { type: String, required: true},
+    category: { type: String, required: true},
+    description: { type: String, required: true},
+    reviews: [reviewSchema],
+    rating: { type: Number },
+    numReviews: {
+        type: Number,
+        default: 0
+    },
+    price: {
+        type: Number,
+        required: true, 
+        default: 0
+    },
+    countInStock: {
+        type: Number, 
+        required: true,
+        default: 0
+    }
+}, {
+    timestamps: true
+});
+
+const Product = mongoose.model('Product', productSchema);
+
+export default Product; 
