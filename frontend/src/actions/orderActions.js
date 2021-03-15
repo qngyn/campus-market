0afdc49d6c4a61c68f2936fab5ffbd9b8ce3bfbd
thiennhanng191 <ntnguyen@vassar.edu>
2021-03-14 20:58:51 -0400

    added order details reducer and action

--- frontend/src/actions/orderActions.js
+++ frontend/src/actions/orderActions.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { ORDER_CREATE_FAIL, ORDER_CREATE_REQUEST, ORDER_CREATE_SUCCESS } from "../contstants/orderConstants";
+import { ORDER_CREATE_FAIL, ORDER_CREATE_REQUEST, ORDER_CREATE_SUCCESS, ORDER_DETAILS_FAIL, ORDER_DETAILS_REQUEST, ORDER_DETAILS_SUCCESS } from "../contstants/orderConstants";
 
 export const createOrder = (order) => async (dispatch, getState) => { // id here stands for 'profile' -- subject to change
     try {
@@ -25,6 +25,35 @@ export const createOrder = (order) => async (dispatch, getState) => { // id here
     } catch (error) {
         dispatch({
             type: ORDER_CREATE_FAIL,
+            payload: error.response && error.response.data.message ? // error.response.data.message is the custom error message sent from the backend
+                error.response.data.message : error.message
+        });
+    }
+}
+
+export const getOrderDetails = (id) => async (dispatch, getState) => { // id here stands for 'profile' -- subject to change
+    try {
+        dispatch({
+            type: ORDER_DETAILS_REQUEST
+        });
+        
+        const { userLogin: { userInfo } } = getState();
+
+        const config = {
+            headers: {
+                Authorization: `Bearer ${userInfo.token}`
+            }
+        }
+        const { data } = await axios.get(`/api/orders/${id}`, config);
+
+        dispatch({
+            type: ORDER_DETAILS_SUCCESS,
+            payload: data
+        });
+
+    } catch (error) {
+        dispatch({
+            type: ORDER_DETAILS_FAIL,
             payload: error.response && error.response.data.message ? // error.response.data.message is the custom error message sent from the backend
                 error.response.data.message : error.message
         });
